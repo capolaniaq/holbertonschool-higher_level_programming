d4334ac21928ef2116dcd4f952330adf0658f3cd
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-04-27 17:34:05 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,11 @@ else:
 last = number2 % 10
 print("Last digit of", end='')
 if number < 0:
+    lastnegative = last * -1
     if last > 5:
-        print(" {:d} is -{:d} and is greater than 5".format(number, last))
+        print(" {:d} is {:d} and is greater than 5".format(number, lastnegative))
     elif last < 6 and last != 0:
-        print(" {:d} is -{:d} and less than 6 and not 0".format(number, last))
+        print(" {:d} is {:d} and less than 6 and not 0".format(number, lastnegative))
     else:
         print(" {:d} is {:d} and is 0".format(number, last))
 else:
