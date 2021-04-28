bad5fb67225393cee6b9f3ebd1dc2f04b18b471b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-04-28 23:21:46 +0000

    correc the first argument in word argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("{} Arguments.".format(0))
+        print("{} arguments.".format(len(sys.argv) - 1))
     elif len(sys.argv) == 2:
-        print("{} Argument:".format(len(sys.argv) - 1))
+        print("{} argument:".format(len(sys.argv) - 1))
     else:
-        print("{} Arguments:".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i, argv in enumerate(sys.argv):
         if i > 0:
             print("{}: {}".format(i, argv))
