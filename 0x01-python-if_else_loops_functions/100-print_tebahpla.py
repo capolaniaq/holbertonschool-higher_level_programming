2c6cba6a7f6f6d2ed8fc55fd90439250e6d62191
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-04-28 02:24:03 +0000

    100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        i = i
+    else:
+        i = i - 32
+    print("{}".format(chr(i)), end='')
+    if i < 97:
+        i = i + 32
