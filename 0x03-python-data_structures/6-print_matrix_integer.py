c45e7996c7b26360d5a7d3a0d60b81188dbf884f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-07 23:22:46 +0000

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i, row in enumerate(matrix):
-        print("{:d}".format(matrix[i]))
+        for x, number in enumerate(row):
+            print("{:d}".format(number), end="")
+            if x < len(row) - 1:
+                print(end=" ")
+        print()
