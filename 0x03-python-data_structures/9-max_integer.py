4cf8d74a11dfcfac82fdd2d1439109e773338ebe
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-08 02:37:39 +0000

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    sort_list = sorted(my_list)
+    if len(my_list) == 0:
+        return None
+    else:
+        sort_list = sorted(my_list)
     return sort_list[len(my_list) - 1]
