45759caa20ededfa00f8313790a5680e17a4ec28
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-17 18:34:26 -0500

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        my_list is None
+    except Exception:
+        return 0
+    for i, j in enumerate(my_list):
+            if i == x:
+                print("")
+                return i
+            else:
+                print("{}".format(my_list[i]), end="")
+    if (i <= x):
+        print("")
+        return i + 1
