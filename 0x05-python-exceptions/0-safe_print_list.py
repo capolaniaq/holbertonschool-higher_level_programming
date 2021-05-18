322ded5819ef9d1ebf893e901f4b61bebeae16fa
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-17 19:12:29 -0500

    fixing case empty list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,13 +3,14 @@ def safe_print_list(my_list=[], x=0):
     try:
         my_list[0] is None
     except Exception:
+        print("")
         return 0
     for i, j in enumerate(my_list):
         if i == x:
             print("")
             return i
         else:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(j), end="")
     if (i <= x):
         print("")
         return i + 1
