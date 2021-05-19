0c1ad625c0a835024a34568a1d619c55335b4006
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-19 15:01:34 -0500

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that declare class Square with a private instance attribute size:"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """Function to validade of the size square"""
+        self.__size = size
+        try:
+            if size < 0:
+                print("size must be >= 0")
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+            raise TypeError
