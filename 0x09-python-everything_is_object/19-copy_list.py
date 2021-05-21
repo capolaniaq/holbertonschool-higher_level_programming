23f22fbd1bd36c72bd686b6fba28b067b4491385
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-21 18:53:34 -0500

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-"""Module that copy  a list i a new list"""
-
-
 def copy_list(l):
-    """Function that copy a list in the new list"""
-    new = l
-    return new
+    return l + []
