4940464c7bfc2906f8b2d68b1f6226ba03dc1a2b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-20 23:08:34 -0500

    1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module of define a empty class Rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Function to validade of the width and height of rectangle"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Function that validade fo the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Function that validade fo the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Function that validade fo the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Function that validade fo the height"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
