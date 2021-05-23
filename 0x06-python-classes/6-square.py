a4792a359de20618bf63ee81281f6e309b14b9ec
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-23 18:58:19 -0500

    type error in  case tuple empty in position 0 or 1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,13 +31,13 @@ class Square:
 
     @position.setter
     def position(self, value):
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
