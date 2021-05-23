eb9bd8fcec9824ee55f2953611176b1c5eae5a41
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-23 18:33:19 -0500

    fixing edges cases of 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Function to validade of the size square"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
