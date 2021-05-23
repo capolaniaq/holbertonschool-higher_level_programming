cc7976dd2829652bf2b721265c74d87f8b0de6ed
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-22 23:58:33 -0500

    fixing the none case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     value_max = max(a_dictionary.values())
-    if value_max == None:
-        return None
     for i in a_dictionary:
         if a_dictionary[i] == value_max:
             key_max = i
