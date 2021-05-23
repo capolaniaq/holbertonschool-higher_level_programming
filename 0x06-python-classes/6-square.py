41d578a814bdf4a84ce49e853ea7a155cd0dad05
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-23 18:47:27 -0500

    type error in  case tuple empty in position 0 or 1

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,6 +37,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] is None or value[1] is None:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
