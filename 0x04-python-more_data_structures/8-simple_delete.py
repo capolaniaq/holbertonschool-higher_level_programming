5c65dfd47a2251d480e673cfea010a801465e711
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-22 22:44:21 -0500

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dictionary = a_dictionary
     if key in a_dictionary:
-        new_dictionary = new_dictionary.pop(key)
-    return new_dictionary
+        del a_dictionary[key]
+    return a_dictionary
