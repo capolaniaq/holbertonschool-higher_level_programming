7ce2ddb396e017cdf5afc87d59902ae300dfa7ed
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-22 23:51:01 -0500

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     value_max = max(a_dictionary.values())
+    if value_max == None:
+        return None
     for i in a_dictionary:
         if a_dictionary[i] == value_max:
             key_max = i
