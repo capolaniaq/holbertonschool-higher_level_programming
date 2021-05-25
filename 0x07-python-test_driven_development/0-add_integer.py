268211848ae2d48ac1582f7970c55fba6d126219
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-24 22:06:05 -0500

    fixing that error message

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Module that contain the fucntion add_integer"""
+"""
+Module that contain the fucntion add_integer
+"""
 
 
 def add_integer(a, b=98):
-    """function that add two numbers"""
-
+    """
+    function that add two numbers
+    Return the result in datatype
+    Raise typeError if data is different that int
+    """
     if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer or b must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not float and type(b) is not int:
-        raise TypeError("b must be an integer or b must be an integer")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
