c9f592456c1ff33bc4e3ee79d7e101b6b12665cb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-31 16:53:19 -0500

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """Function that print the content of the my_file_0.txt"""
 
-    with open(filename, "r") as myfile:
+    with open(filename, "r", encoding="utf-8") as myfile:
         print(myfile.read())
