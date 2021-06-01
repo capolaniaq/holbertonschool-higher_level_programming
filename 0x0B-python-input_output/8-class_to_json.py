d564700d67a905ba5cb3aa4039110e2486eaeba1
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-05-31 23:08:00 -0500

    fixing import module

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Module that Convert to instance class to json dict"""
-import json
-
 
 def class_to_json(obj):
     """Function that convert to Instance class to json"""
 
-    obj = json.dumps(obj.__dict__)
+    obj = obj.__dict__
     return obj
