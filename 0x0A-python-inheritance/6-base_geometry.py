90bf80fbd5562d47e777aac862ecdbf44aba56d7
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-05 18:07:02 -0500

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that create a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Function that declare Class BaseGeometry"""
+
+    def area(self):
+        """Function that raise exception of the
+        public method area"""
+
+        raise Exception("area() is not implemented")
