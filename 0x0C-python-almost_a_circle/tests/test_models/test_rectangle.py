d2639fcd934e8e8af51d7f4f0f573a0e72282a05
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-08 15:28:52 -0500

    tests/test_models/test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,4 +13,11 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual((b3.width, b3.height, b3.x, b3.y, b3.id), (10, 12, 1, 1, 3))
         b4 = Rectangle(10, 12, 1, 1, 5)
         self.assertEqual((b4.width, b4.height, b4.x, b4.y, b4.id), (10, 12, 1, 1, 5))
+        b5 = Rectangle(3, 2)
+        self.assertEqual((b5.area()), (6))
+        b6 = Rectangle(2, 10)
+        self.assertEqual(b6.area(), 20)
+        b7 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(b7.area(), 56)
+
 
