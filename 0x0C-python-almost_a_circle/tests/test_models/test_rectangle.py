bcd8d8c8dbc591f4b5060982c3848a3f4fd73075
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-08 15:59:28 -0500

    tests/test_models/test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,13 +10,25 @@ class TestRectangle(unittest.TestCase):
     def test_equal(self):
         """test_equal for a each option to rectbagle"""
         b1 = Rectangle(10, 12)
-        self.assertEqual((b1.width, b1.height, b1.id), (10, 12, 1))
+        self.assertEqual((b1.width, b1.height, b1.id), (10, 12, 4))
+
+    def test_x(self):
+        """test_equal for a each option to rectbagle"""
         b2 = Rectangle(10, 12, 1)
-        self.assertEqual((b2.x, b2.id), (1, 2))
+        self.assertEqual((b2.x, b2.id), (1, 5))
+
+    def test_correct_value(self):
+        """test_equal for a each option to rectbagle"""
         b3 = Rectangle(10, 12, 1, 1)
         self.assertEqual((b3.width, b3.height, b3.x, b3.y), (10, 12, 1, 1))
+
+    def test_id(self):
+        """test_equal for a each option to rectbagle"""
         b4 = Rectangle(10, 12, 1, 1, 5)
         self.assertEqual((b4.x, b4.y, b4.id), (1, 1, 5))
+
+    def test_area(self):
+        """test_area public method"""
         b5 = Rectangle(3, 2)
         self.assertEqual((b5.area()), (6))
         b6 = Rectangle(2, 10)
