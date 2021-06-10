a3f9381a35844fb47a2572f7cd944bce40cd3366
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-09 22:07:26 -0500

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,24 @@ class Square(Rectangle):
         """Fucntion setter that size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Function that update the attributes that args"""
+        for position, arg in enumerate(args):
+            if position == 0:
+                self.id = arg
+            elif position == 1:
+                self.width = arg
+            elif position == 2:
+                self.x = arg
+            else:
+                self.y = arg
+        for key, value in kwargs.items():
+            if key == 'id':
+                self.id = value
+            elif key == 'size':
+                self.width = value
+            elif key == 'x':
+                self.x = value
+            elif key == 'y':
+                self.y = value
