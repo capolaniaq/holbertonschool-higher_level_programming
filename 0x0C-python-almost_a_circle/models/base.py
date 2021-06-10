05cd27aba001242e91d8018ec7a6fbc8dabaf159
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-09 22:50:57 -0500

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module that created a Class Base"""
 
+import json
+
 
 class Base:
     """Class Base"""
@@ -12,3 +14,9 @@ class Base:
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Function that return a JSON object as string"""
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
