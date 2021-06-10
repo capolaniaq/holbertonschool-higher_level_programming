2324ea39e833e3ec32b9373748d325c3b50ce977
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-09 23:09:12 -0500

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,6 +15,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Function that return a JSON object as string"""
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
