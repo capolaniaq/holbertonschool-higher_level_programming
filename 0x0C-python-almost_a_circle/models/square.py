fb73407fefc09646aed9fae5becee5aa94d8824a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-09 19:04:13 -0500

    models/square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that created a Class Rectangle"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """instations Rectangle"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Function that return a str to print"""
+        return ("[Square] ({}) {}/{} - \
+{}".format(self.id, self.x, self.y, self.width))
