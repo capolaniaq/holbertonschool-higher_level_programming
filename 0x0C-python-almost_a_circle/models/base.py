5a7e730446190983514ab4bc67717eb1eb96d6bd
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-06-13 23:32:14 -0500

    save_to_file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,14 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Function that write to JSON OBJECT to file"""
+        list_dict = []
+        file_name = cls.__name__ + '.json'
+        if list_objs:
+            for item in list_objs:
+                list_dict += [item.to_dictionary()]
+        with open(file_name, 'w', encoding='utf-8') as file_open:
+            file_open.write(cls.to_json_string(list_dict))
