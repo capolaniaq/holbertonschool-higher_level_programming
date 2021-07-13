6ac444f0726d1d42be0531c293daec9a1e3f2735
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-13 20:10:04 +0000

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- crete the table with the parameters
+-- if the table exist the script no must fail
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+
+--insert values
+INSERT INTO second_table (id, name, score) VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
