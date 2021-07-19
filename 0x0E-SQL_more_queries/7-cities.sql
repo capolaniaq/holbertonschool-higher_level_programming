0a7f7a778ef0fbb76ad88e29a6ac790621ffe894
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-19 03:06:33 +0000

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
 	PRIMARY KEY(id),
-	FOREING KEY (state_id) REFERENCES states(id)
+	FOREIGN KEY (state_id) REFERENCES states(id)
 );
