c64b3e6de5d13ee3d290bdd9a02b7a7c551b53eb
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-07-19 03:30:50 +0000

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
--- create a database
--- create a table with the primary key
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities` (
-	`id` INT PRIMARY KEY AUTO_INCREMENT,
+-- create a database 
+-- create a table with the primary kiiey
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	`state_id` INT NOT NULL,
-	FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
+	PRIMARY KEY (id),
+	CONSTRAINT cities_ibfk_1 FOREIGN KEY (state_id) REFERENCES states(id)
 );
