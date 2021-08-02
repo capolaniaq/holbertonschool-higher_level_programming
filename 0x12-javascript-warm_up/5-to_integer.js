021f18bd006196d1923ec6672a922241ff33c913
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-02 17:09:33 -0500

    fixing the error to print my nUmber

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const mynumber = parseInt(process.argv[2]);
 if (isNaN(mynumber)) {
   console.log('Not a number');
 } else {
-  console.log('my number: ' + mynumber);
+  console.log('My number: ' + mynumber);
 }
