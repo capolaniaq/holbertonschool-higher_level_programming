5076b1b2ef867b53035272221ce09755bee1654d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-02 13:57:10 -0500

    fixing the space error

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 const firts = 'C is fun';
 const second = 'Python is cool';
 const third = 'JavaScript is amazing';
-console.log(firts, '\n', second, '\n', third);
+console.log(firts);
+console.log(second);
+console.log(third);
