a44fb907ce8710bf908499e66a38fc02d77d0dce
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-02 16:54:37 -0500

    fixing the no found paramters

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(process.argv[2], 'is', process.argv[3]);
+if (process.argv[2]) {
+  console.log(process.argv[2], 'is', process.argv[3]);
+} else {
+  console.log('undefined is undefined');
+}
