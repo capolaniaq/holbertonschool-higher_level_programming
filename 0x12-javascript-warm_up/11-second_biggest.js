4bca9f03357066aea6a0e6596f69bc6424820a0d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-02 20:18:02 -0500

    fixing error less arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 let args = process.argv.slice(2);
-args = args.sort(function (a, b) { return a - b; });
-console.log(args[args.length - 2]);
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
+} else {
+  args = args.sort(function (a, b) { return a - b; });
+  console.log(args[args.length - 2]);
+}
