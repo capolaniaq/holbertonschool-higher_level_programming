82068c5ede6880afb6fdb10993f8812b57e84e9d
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-04 17:41:36 -0500

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,8 +5,6 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    this.height = size;
-    this.width = size;
   }
 }
 module.exports = Square;
