a4daf21c5ebbe8be4a593fbe0d1adceaf1c46b32
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-06 11:45:19 -0500

    fixing the pep8 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
+"""that lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_us"""
 
 import MySQLdb
 from sys import argv
@@ -8,13 +9,15 @@ if __name__ == "__main__":
     users = argv[1]
     password = argv[2]
     database = argv[3]
-    db = MySQLdb.connect(host="localhost",port=3306,user=users,
-                  passwd=password,db=database, charset="utf8")
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=users,
+        passwd=password, db=database, charset="utf8")
     cursor = db.cursor()
-    sql = "SELECT states.id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    sql = ("SELECT states.id, name FROM states WHERE name LIKE 'N%'\
+    ORDER BY states.id ASC")
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
-        print (row)
+        print(row)
     cursor.close()
     db.close()
