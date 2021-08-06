e0946f4930c4192ea696c82962e89c1d7ac044a3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-06 14:17:43 -0500

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" But this time, write one that is safe from MySQL injections!"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    users = argv[1]
+    password = argv[2]
+    database = argv[3]
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=users,
+        passwd=password, db=database, charset="utf8")
+    cursor = db.cursor()
+    cursor.exucute("SELECT states.id, name FROM states WHERE\
+        name = %(argument)s ORDER BY states.id ASC", {'argument': argv[4]})
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()
