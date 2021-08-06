702c1befa6073c937a942a01e6bdc74ad3678b83
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-06 11:40:26 -0500

    fixing the pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     users = argv[1]
     password = argv[2]
     database = argv[3]
-    db = MySQLdb.connect(host="localhost",port=3306,user=users,
-                  passwd=password,db=database, charset="utf8")
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=users,
+        passwd=password, db=database, charset="utf8")
     cursor = db.cursor()
     sql = "SELECT states.id, name FROM states ORDER BY states.id ASC"
     cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
-        print (row)
+        print(row)
     cursor.close()
     db.close()
