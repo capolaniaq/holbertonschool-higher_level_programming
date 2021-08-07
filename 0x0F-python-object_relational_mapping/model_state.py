7d869e1fd430ff00a4ad5103ce7eae80edf7e45f
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-07 16:13:50 -0500

    fixing error requeriment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,11 +5,16 @@
 from sqlalchemy import create_engine, Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
+engine = create_engine("mysql://root:root@localhost:3306/db")
 Base = declarative_base()
 
 
 class State(Base):
-    """Firts class state inherits to Base"""
+    """firts class state inherits base"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
