9da754877540562029cd0093db8ae8dcf4d29d4b
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-07 14:54:09 -0500

    fixing output errors

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         FROM cities
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC;""")
-    cursor.exucute(sql)
+    cursor.execute(sql)
     results = cursor.fetchall()
     for row in results:
         print(row)
