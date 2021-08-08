c0236de7a24f29c93aab44db3d52c1e5c062e521
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-07 19:31:28 -0500

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+        print("{:d}: {:s}".format(state.id, state.name))
     session.close()
