c3367e9bf973072664de32fb602d3d6cf48e631a
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-20 18:27:40 -0500

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+use the request"""
+
+import requests
+
+
+if __name__ == '__main__':
+    """request with the module request"""
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
