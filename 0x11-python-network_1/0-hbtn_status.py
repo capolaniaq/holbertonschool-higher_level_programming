b3811eb4f86f965ac260505fd5979e81aa327d38
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-20 15:19:20 -0500

    \t not tab

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         print("Body response:")
         content = response.read()
-        print(" - type: {}".format(type(content)))
-        print(" - content: {}".format(content))
-        print(" - utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
