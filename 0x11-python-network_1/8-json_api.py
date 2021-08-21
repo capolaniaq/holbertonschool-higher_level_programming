bc4cf5f98330b4e3714512da21cac35149e5cc00
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-20 20:05:51 -0500

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status
+use the request"""
+
+from json.decoder import JSONDecodeError
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    """request with the module request
+    use the Post request type"""
+    if len(sys.argv) == 2:
+        value = sys.argv[1]
+    else:
+        value = ""
+
+    try:
+        r = requests.post("http://0.0.0.0:5000/search_user", data={'q': value})
+        js = r.json()
+
+        if js:
+            print("[{}] {}".format(js.get('id'), js.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
