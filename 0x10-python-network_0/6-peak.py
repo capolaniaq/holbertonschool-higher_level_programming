17dd9a0606979b2cc08512f8c15eeb0b00e4a5d3
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-23 01:04:35 -0500

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,10 @@ def find_peak(list_of_integers):
 
     if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    peak = 0
+    list_of_integers.sort()
+    peak = list_of_integers[-1]
+    """
     for number in list_of_integers:
         if number > peak:
-            peak = number
+            peak = number"""
     return peak
